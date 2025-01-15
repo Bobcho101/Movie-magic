b8d8af30cac0905a8f9d58d7860d311407a034b1
Bobcho101 <bgbobi878@gmail.com>
2025-01-15 23:11:36 +0200

    settupe the engine and the main path is src

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import express from 'express';
 const app = express();
 const port = 4000;
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
     res.send('Hi');
 });
 
 
-app.listen(port, console.log(`Server is listening to: http://localhost:${port}`));
+app.listen(port, () => console.log(`Server is listening to: http://localhost:${port}`));
