a95f788dfafaf7deb81d21db3eb6e858b49aa575
Bobcho101 <bgbobi878@gmail.com>
2025-01-17 22:20:50 +0200

    rendered movies

--- src/controller/home-controller.js
+++ src/controller/home-controller.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
+import movies from '../data/movies.js';
 
 const homeController = Router();
 
 homeController.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {movies});
 });
 
-homeController.get('/create', (req, res) => {
-    res.render('create')
-});
+
 
 export default homeController;
