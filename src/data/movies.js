f86b8f3647227f37d32a1c332109acbef5665ae9
Bobcho101 <bgbobi878@gmail.com>
2025-01-17 22:20:37 +0200

    created a mini database with an object

---
+++ src/data/movies.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        id: 'dw32dw-dag843-dawd2da',
+        title: 'Anabel',
+        category: 'Movie',
+        genre: 'Horror',
+        director: 'Random Person',
+        year: '1995',
+        moviePoster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbAvXsZWOfqzsf_v0HmiaIASOPuuRnvA76Iw&s',
+        rating: '6',
+        description: 'Annabelle is a 2014 American supernatural horror film directed by John R. Leonetti, written by Gary Dauberman and produced by Peter Safran and James Wan.'
+    }
+]
