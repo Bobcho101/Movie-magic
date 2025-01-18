3a1390d4e33bd16d55bf081318df9d73b389108d
Bobcho101 <bgbobi878@gmail.com>
2025-01-18 10:47:00 +0200

    i handled the id from the params and got its info

---
+++ src/controller/details-controller.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getOne } from "../data/db-methods.js";
+
+const detailsController = Router();
+
+detailsController.get('/details/:movieId', (req, res) => {
+    const currentId = req.params.movieId;
+    const currentMovie = getOne(currentId);
+
+    res.render();
+});
+
+export default detailsController;
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import homeController from './controller/home-controller.js';
 import aboutController from './controller/about-controller.js';
 import createController from './controller/create-controller.js';
+import detailsController from './controller/details-controller.js';
 
 const routes = Router();
 
 routes.use(homeController);
 routes.use(aboutController);
 routes.use(createController);
+routes.use(detailsController);
 
 routes.get('*', (req, res) => {
     res.render('404');
