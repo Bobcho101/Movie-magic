5d30b330a06d0d50fc93c967242b1388eda039c3
Bobcho101 <bgbobi878@gmail.com>
2025-01-19 12:44:55 +0200

    title filter

--- src/data/db-methods.js
+++ src/data/db-methods.js
@@ -4,3 +4,12 @@ export function getOne(id) {
     const movie = movies.find(movie => movie.id === id);
     return movie;
 }
+export function getAllFilteredMovies(filter = {}){
+    let result = movies;
+    if(filter.search){
+        result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
+    }
+    
+
+    return result;
+}
