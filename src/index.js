eb520bdf49e6a1f4d0f70ab086e00c52ed8ec4ce
Bobcho101 <bgbobi878@gmail.com>
2025-01-23 19:28:26 +0200

    mongo db connection setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'; 
 import handlebars from 'express-handlebars';
+import mongoose from 'mongoose';
 import routes from './routes.js';
 const app = express();
 const port = 4000;
@@ -13,8 +14,17 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');  
 
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('src/public'));
+
+try{
+    const uri = 'mongodb://127.0.0.1/Movie Magic';
+    await mongoose.connect(uri);
+} catch(err){
+    console.log(err.message);
+}
+
 app.use(routes);
 
 app.listen(port, () => console.log(`Server is listening to: http://localhost:${port}`));
