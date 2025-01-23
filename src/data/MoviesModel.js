d13789b8aafff29dcc8a6cc181dda29d3d2d4698
Bobcho101 <bgbobi878@gmail.com>
2025-01-23 19:33:15 +0200

    created a movie schema and model

--- src/data/MoviesModel.js
+++ src/data/MoviesModel.js
@@ -1,5 +1,17 @@
-import mongoose, { Schema, mode } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 
 
+const movieSchema = new Schema({
+    title: String,
+    category: String,
+    genre: String,
+    director: String,
+    year: Number,
+    imageUrl: String,
+    rating: Number,
+    description: String,
+});
 
+const Movie = model('Movie', movieSchema);
 
+export default Movie;
