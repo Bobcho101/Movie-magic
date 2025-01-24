70cd67cb084d61bd953bf62c77364456fce0cd66
Bobcho101 <bgbobi878@gmail.com>
2025-01-24 17:22:13 +0200

    rendered the current movie in the attach cast page

--- src/controller/attach-cast-controller.js
+++ src/controller/attach-cast-controller.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import Cast from "../models/CastsModel.js";
+import Movie from "../models/MoviesModel.js";
 
 const attachCastController = Router();
 
 attachCastController.get('/details/:movieId/attach-cast', async (req, res) => {
     const casts = await Cast.find({});
+    const movie = await Movie.findById(req.params.movieId);
+   
     
-    res.render('movie/cast-attach', {casts});
+    res.render('movie/cast-attach', { casts, movie });
 });
 
 export default attachCastController;
