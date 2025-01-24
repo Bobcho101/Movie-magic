e3126efcc0a9a0ec54aeb436a7308c0429d53833
Bobcho101 <bgbobi878@gmail.com>
2025-01-24 17:18:48 +0200

    rendered the casts in the attach options

--- src/controller/attach-cast-controller.js
+++ src/controller/attach-cast-controller.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import Cast from "../models/CastsModel.js";
 
 const attachCastController = Router();
 
-attachCastController.get('/details/:movieId/attach-cast', (req, res) => {
-    res.render('cast-attach');
+attachCastController.get('/details/:movieId/attach-cast', async (req, res) => {
+    const casts = await Cast.find({});
+    
+    res.render('movie/cast-attach', {casts});
 });
 
 export default attachCastController;
