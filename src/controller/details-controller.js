198dfd7e092004cf6708e932bac9dbcc9f47fb09
Bobcho101 <bgbobi878@gmail.com>
2025-01-25 15:59:07 +0200

    rendered the every movies' casts

--- src/controller/details-controller.js
+++ src/controller/details-controller.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { getOne } from "../services/movie-services.js";
+import { getCastsForRender } from "../services/cast-services.js";
 
 const detailsController = Router();
 
 detailsController.get('/details/:movieId', async (req, res) => {
     const currentId = req.params.movieId;
     const currentMovie = await getOne(currentId);
+    const currentMovieCastsIds = currentMovie.casts;
+    const casts = await getCastsForRender(currentMovieCastsIds);
 
+    
     const rating = Math.floor(currentMovie.rating);
 
   
@@ -16,7 +20,7 @@ detailsController.get('/details/:movieId', async (req, res) => {
     }
 
     
-    res.render('movie/movie-details', { currentMovie, stars });
+    res.render('movie/movie-details', { currentMovie, stars, casts});
 });
 
 export default detailsController;
