591fe31db9b0843cc547877a1303df433c2d7ebf
Bobcho101 <bgbobi878@gmail.com>
2025-01-29 23:31:04 +0200

    fixed an error
    typed send instead of render

--- src/controller/login-controller.js
+++ src/controller/login-controller.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const loginController = Router();
 
 loginController.get('/auth/login', (req, res) => {
-    res.send('auth/login');
+    res.render('auth/login');
 });
 
 export default loginController;
