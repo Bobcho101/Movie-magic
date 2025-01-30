dab3aec39b60792294ebc8083dde7f1950f4ab7b
Bobcho101 <bgbobi878@gmail.com>
2025-01-30 11:19:51 +0200

    now express can you cookie parser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express'; 
+import cookieParser from 'cookie-parser';
 import handlebars from 'express-handlebars';
 import mongoose from 'mongoose';
 import routes from './routes.js';
@@ -19,6 +20,7 @@ app.set('views', 'src/views');
 
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static('src/public'));
 
 try{ 
