a77f4d1595e15a5856f6c61d0b5903c5814ba931
Bobcho101 <bgbobi878@gmail.com>
2025-02-04 17:11:51 +0200

    added it for create cast too

--- src/controllers/create-cast-controller.js
+++ src/controllers/create-cast-controller.js
@@ -15,7 +15,8 @@ createCastController.post('/create-cast', async (req, res) => {
         res.redirect('/');
     } catch(err){
         console.log(err.message);
-        res.redirect('/404');
+        res.render('create/cast-create', { error: err.message })
+        //res.redirect('/404');
     }
     
 });
