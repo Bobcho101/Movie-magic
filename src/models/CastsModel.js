60a58f4be7ecefe0969b2b4fef37aed13eaf926a
Bobcho101 <bgbobi878@gmail.com>
2025-02-04 16:35:18 +0200

    added validations for cast schema

--- src/models/CastsModel.js
+++ src/models/CastsModel.js
@@ -3,25 +3,30 @@ import mongoose, { Schema, model } from "mongoose";
 const castSchema = new Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Name is required!'],
+        minLength: [5, 'Name should be at least 5 characters long!'],
+        match: [/^[a-zA-Z 0-9]+$/, 'Name should be alphanumeric, digits and whitespaces only!']
     },
     age: {
         type: Number,
-        required: true,
-        min: [0, 'Cannot be less than 0!'],
-        max: [100, "That's too old!"]
+        required: [true, 'Age is required!'],
+        min: [1, 'Cannot be less than 0!'],
+        max: [120, "Max age is 120!"]
     },
     born: {
         type: String,
-        required: true,
+        required: [true, 'Born location is required!'],
+        minLength: 10,
+        match: /^[a-zA-Z 0-9]+$/,
     },
     nameInMovie: {
         type: String,
-        required: true
+        required: [true, 'Name in movie is required!']
     },
     castImage: {
         type: String,
-        required: true
+        required: [true, 'Cast image is required!'],
+        match: /^https?:\/\//,
     }
 });
 
